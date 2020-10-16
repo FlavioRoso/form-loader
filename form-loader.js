3df310ca84e0ebafc19129b6f005137ef4be34a3
FlavioRoso <flavio.roso@gmail.com>
2020-10-16 12:28:29 -0300

    Correção de bug suporte a Select, Radio e Checkbox

--- form-loader.js
+++ form-loader.js
@@ -1,10 +1,5 @@
 const formLoader = {
 
-    state:{
-        form: undefined,
-        values: undefined,
-    },
-
 
     submit: function(form,callback){
 
@@ -37,13 +32,27 @@ const formLoader = {
 
     },
 
+    isInputData: function(elem) {
+
+        
+
+        return 
+
+    },
+
     getInputsForm: function(form)  {
 
         const node = form.childNodes;
         let inputs = [];
+
+        const tags = [
+            "INPUT",
+            "SELECT"
+        ]
+
         Array.prototype.map.call(node,function(elem){
       
-            if(elem instanceof Element && elem.tagName === "INPUT")
+            if(elem instanceof Element &&  tags.indexOf(elem.tagName) >= 0)
             {
                 
                 inputs.push(elem);
@@ -58,14 +67,47 @@ const formLoader = {
 
     },
 
+  
+
     getInputData: function (form) {
 
         let inputs = {}
+
+        const typesData = {
+
+            "radio" : function(obj,elem){
+
+                if(elem.checked)
+                     obj[elem.name] =  elem.value ;
+
+            },
+            "checkbox": function(obj,elem){
+
+                if(obj[elem.name] == undefined)
+                    obj[elem.name] = [];
+
+                if(elem.checked)
+                     obj[elem.name].push(elem.value);
+
+            }
+            
+        }
         
         const nodeInputs = this.getInputsForm(form);
 
         nodeInputs.forEach((elem) => {
-            inputs[elem.name] = elem.value;
+
+            
+            if(typesData[elem.type] !== undefined)
+            {
+                typesData[elem.type](inputs,elem)
+            }
+                
+            else
+            {
+                inputs[elem.name] = elem.value;
+            }
+                
         });
 
 
